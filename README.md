b92857d107985eac44eccb719c22ddbff1708e76
Paul Heading <paul.heading@barbican.org.uk>
2021-02-19 09:52:33 +0000

    amend wording

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TreehouseTaskSeven
 
+I combined Node.js and Express on the server side with Pug templates to create a portfolio site, showcasing the treehouse projects that I've built so far.
+
 ![screengrab](https://repository-images.githubusercontent.com/160225919/4055f200-7295-11eb-8c16-fef9b11d39fb)
--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-
-const data = require('./data.json');
-const express = require('express');
-const pug = require('pug');
+const data = require("./data.json");
+const express = require("express");
+const pug = require("pug");
 
 const app = express();
 const port = 3000;
 
-app.set('view engine','pug');
-app.use('/public/',express.static('public'));
+app.set("view engine", "pug");
+app.use("/public/", express.static("public"));
 
 app.locals.name = "Paul Heading";
 app.locals.intro = "I am Paul Heading and I am awesome for figuring this out!";
-app.locals.description = "This site is meant to showcase my treehouse projects. It's providing really good practice working with the pug templating system.";
+app.locals.description =
+  "This site is meant to showcase my treehouse projects. It's providing really good practice working with the pug templating system.";
 app.locals.data = data.projects;
 
 app.listen(port, () => {
@@ -19,20 +19,20 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => res.render("index"));
-app.get('/about',(req,res) => res.render('about'));
+app.get("/about", (req, res) => res.render("about"));
 
-app.get(`/project/:id`, function(req, res){
-  res.render('project', { id: req.params.id });
+app.get(`/project/:id`, function (req, res) {
+  res.render("project", { id: req.params.id });
 });
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   const err = new Error("page not found :(");
   err.status = 404;
   next(err);
 });
 
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
-  res.render('error');
+  res.render("error");
 });
